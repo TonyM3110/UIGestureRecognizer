beb6dd9ec110a28e5b79ec1a210481f3ff56fa1c
Tony Cherevko <anton.ch.tr@gmail.com>
2022-03-19 09:04:19 +0700

    Initial Commit

---
+++ UIGestureRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIGestureRecognizer
+//
+//  Created by Tony on 19.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
