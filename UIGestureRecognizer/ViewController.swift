3bd93658855dc6bb1f0fc446e10e052f55c17c80
Tony Cherevko <anton.ch.tr@gmail.com>
2022-03-19 09:20:44 +0700

    UIGestureRecognizer
    Наблюдатель за свайпами и тапами

--- UIGestureRecognizer/ViewController.swift
+++ UIGestureRecognizer/ViewController.swift
@@ -9,11 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        swipeObservers()
+        tapObserver()
     }
-
-
+    
+    func swipeObservers() {
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeUP = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeUP.direction = .up
+        self.view.addGestureRecognizer(swipeUP)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
+    }
+    
+    func tapObserver() {
+        let trippleTap = UITapGestureRecognizer(target: self, action: #selector(trippleTapAction))
+        trippleTap.numberOfTapsRequired = 3
+        self.view.addGestureRecognizer(trippleTap)
+        
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(doubleTappAction))
+        doubleTap.require(toFail: trippleTap)
+        doubleTap.numberOfTapsRequired = 2
+        self.view.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func handleSwipes(gester: UISwipeGestureRecognizer) {
+        
+        switch gester.direction {
+        case .right:
+            labelText.text = "Right Swipe"
+        case .left:
+            labelText.text = "Left Swipe"
+        case .up:
+            labelText.text = "Up Swipe"
+        case .down:
+            labelText.text = "Down Swipe"
+        default:
+            break
+        }
+    }
+    
+    @objc func trippleTapAction() {
+        labelText.text = "Tripple Tap"
+    }
+    
+    @objc func doubleTappAction() {
+        labelText.text = "Double Tap"
+    }
 }
 
